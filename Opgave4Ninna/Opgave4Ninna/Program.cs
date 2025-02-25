e83019fcd09083f819bf8285e36eff5a0b780ddb
Ninna Lundberg <nllu71404@edu.ucl.dk>
2025-02-25 13:01:43 +0100

    Ninnas besvarelse af opgave 4

---
+++ Opgave4Ninna/Opgave4Ninna/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Opgave4Ninna
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Beregninger 
+            double sum = 10 + 5;
+            double difference = 10 - 5;
+            double product = 10 * 5;
+            double quotient = 10 / 5;
+            double remainder = 10 % 5;
+
+            //Output: Print af resultater
+            Console.WriteLine($"Sum: {sum}, Difference: {difference}, Product: {product}, Quotient: {quotient}, Remainder: {remainder}");
+        }
+    }
+}
